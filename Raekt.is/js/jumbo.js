e2221b2c57b2703bfb9bfe17936d61a9045cde1d
palarn <palarn@gmail.com>
2015-03-07 23:24:48 +0000

    Background dóti breytt/bætt við
    Static bakgrunnur í "jumbotron" í mobile. Slideshow í desktop

---
+++ Raekt.is/js/jumbo.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$window = $(window);
+	var jumboHeight = $('.jumbotron').outerHeight();
+	function parallax(){
+	    var scrolled = $(window).scrollTop();
+	    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+	}
+
+	$(window).scroll(function(e){
+	    parallax();
+	});
+
+	
+});
