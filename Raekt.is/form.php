fa35f5c2e134625e48f41853335e496216b87cf5
helgaloa <helgaloa@gmail.com>
2015-03-08 19:36:07 +0000

    hafa samband
    ekki endanleg útgáfa

---
+++ Raekt.is/form.php
@@ -0,0 +1,20 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: Raekt.is'; 
+    $to = 'hlk2@hi.is'; 
+    $subject = 'Email Inquiry';
+
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+?>
+
+<?php
+if ($_POST['submit']) {
+    if (mail ($to, $subject, $body, $from)) { 
+        echo '<p>Takk fyrir ad hafa samband!</p>';
+    } else { 
+        echo '<p>OOps! Thad virdist hafa komid upp villa. Profadu aftur.</p>'; 
+    }
+}
+?>
