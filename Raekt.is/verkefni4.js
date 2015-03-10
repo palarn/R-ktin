ee9a67f1b35f7608f6155d194e84a00869aa79fa
hrafnkellp <hrafnkellp@bitbucket.org>
2015-03-10 16:08:53 +0000

    Æronno
    Einhver samanburður

--- Raekt.is/verkefni4.js
+++ Raekt.is/verkefni4.js
@@ -7,17 +7,26 @@ $(document).ready(function() {
 		{ 
 			var el = $('<p>');
 			el.append('<a href="#" id=' + i + '><p>'+ data[i].title +'</p></a>');
-			el.append('<p><input type="checkbox" id='+ data[i].id + i +' name="myCheckbox"/>&nbsp;&nbsp;&nbsp;Bera saman</p>');
+			el.append('<div><class="checkbox" id='+ data[i].id + i +' name="myCheckbox"/><label><input type="checkbox"></label>Bera saman</div>');
 			$('#listi').append(el);
 		}
+
+
+		
 		$('#listi p').click(function(event) {
 			event.preventDefault();
+
 			if (count==0) {
 				$('#upplysingar').empty();
 			}
 			if (count==0 || count==1) {
 				$('#upplysingar2').empty();
 			}
+			if (count==0 || count==1 || count==2) {
+				$('#upplysingar3').empty();
+			}
+
+
 			var itemId = $(event.currentTarget).children('a').attr('id');
 			$.getJSON('details/'+ stodvar[itemId].id+'.json',function(detailData){
 				var html = '<div class="stodvar">';					  
@@ -29,12 +38,23 @@ $(document).ready(function() {
 					html += '<div class="stadsetning">' + '<p>Staðsetning:'+ detailData['stadsetning'] + '</p>'+'</div>';
 					html += '<div class="hoptimar">' + '<p>Fjöldi hóptíma:'+ detailData['hoptimar'] + '</p>'+'</div>';
 					html += '<div class="staerd">' + '<p>Stærð í fermetrum:'+ detailData['staerd'] + '</p>'+'</div>';
+
+			var mismunur = $(event.currentTarget).children('a').attr('id');
+			$.getJSON('details/'+ stodvar[mismunur].id+'.json',function(detailData){
+				var gogn = '<div class="stodvar">';
+					gogn += '<h3>Mismunur</h3>';
+					gogn += '<div class="1man" id="upplysingar">'+'<p>1 mánuður:' + detailData['1man']-detailData['1man']+' krónur'+ '</p>'+'</div>';
+
 			if (count==0) {
 				$('#upplysingar').append(html);			
 				}
 			if (count==1) {
 				$('#upplysingar2').append(html);				
 				}
+			if (count==1) {
+				$('#upplysingar3').append(gogn);				
+				}
+
 			if (count<1) {
 				count++;
 				}
@@ -43,5 +63,6 @@ $(document).ready(function() {
 		});	
 		return false;
 	});
+  });
 });
 
