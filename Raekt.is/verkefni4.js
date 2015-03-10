e83adf60a8a7d8a830b294cee9f154843f73b09a
hrafnkellp <hrafnkellp@bitbucket.org>
2015-03-10 16:56:00 +0000

    smá meira
    fleiri samanburðir

--- Raekt.is/verkefni4.js
+++ Raekt.is/verkefni4.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	var stodvar;
 	var count = 0;
-	var amount1, amount2;
+	var amount1, amount2, amount3, amount4, amount5, amount6, amount7, amount8;
 	$.getJSON('stodvar.json',function(data){
 		stodvar = data;
 		for (var i=0; i<data.length; i++)
@@ -41,15 +41,25 @@ $(document).ready(function() {
 					html += '<div class="staerd">' + '<p>Stærð í fermetrum:'+ detailData['staerd'] + '</p>'+'</div>';
 				if (count==0) {
 					amount1=detailData['1man'];
+					amount3=detailData['3man'];
+					amount5=detailData['6man'];
+					amount7=detailData['arskort'];
 				}
 				if (count==1) {
 					amount2=detailData['1man'];
+					amount4=detailData['3man'];
+					amount6=detailData['6man'];
+					amount8=detailData['arskort'];
 				}
+
 			var mismunur = $(event.currentTarget).children('a').attr('id');
 			$.getJSON('details/'+ stodvar[mismunur].id+'.json',function(detailData){
 				var gogn = '<div class="stodvar">';
 					gogn += '<h3>Mismunur</h3>';
 					gogn += '<div class="1man" id="upplysingar">'+'<p>1 mánuður:' + (amount1-amount2) +' krónur'+ '</p>'+'</div>';
+					gogn += '<div class="3man" id="upplysingar">'+'<p>3 mánuður:' + (amount3-amount4) +' krónur'+ '</p>'+'</div>';
+					gogn += '<div class="6man" id="upplysingar">'+'<p>6 mánuður:' + (amount5-amount6) +' krónur'+ '</p>'+'</div>';
+					gogn += '<div class="arskort" id="upplysingar">'+'<p>Árskort:' + (amount7-amount8) +' krónur'+ '</p>'+'</div>';
 
 			if (count==0) {
 				$('#upplysingar').append(html);			
