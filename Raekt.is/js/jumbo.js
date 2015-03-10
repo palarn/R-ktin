7fbff001f8b74f30d5b2eff3a711022edfbbbd1f
Páll Arnar <palarn@gmail.com>
2015-03-10 17:48:34 +0000

    °Komment á jumbo.js

--- Raekt.is/js/jumbo.js
+++ Raekt.is/js/jumbo.js
@@ -1,11 +1,13 @@
+//Virkni á mynd á upphafsíðu
 $(document).ready(function(){
 	$window = $(window);
 	var jumboHeight = $('.jumbotron').outerHeight();
+	//Fall sem notar jQuery til þess að láta slideshow/mynd á forsíðu scrollast hægar en restin af síðunni
 	function parallax(){
 	    var scrolled = $(window).scrollTop();
 	    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
 	}
-
+	//Þegar glugga er scrollað er kallað á parallax fallið
 	$(window).scroll(function(e){
 	    parallax();
 	});
