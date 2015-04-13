5711bec973234e0817502e21a4ecfda35314547b
Páll Arnar <palarn@gmail.com>
2015-04-13 14:30:28 +0000

    Smá breytingar í index

--- Raekt.is/index.php
+++ Raekt.is/index.php
@@ -14,12 +14,12 @@
 
 		<div class="bg">
 			<ul class="cb-slideshow">
-            <li><span>Image 01</span><div><h3>BYRJAÐU</h3></div></li>
-            <li><span>Image 02</span><div><h3>STRAX</h3></div></li>
-            <li><span>Image 03</span><div><h3>Í DAG!</h3></div></li>
-            <li><span>Image 04</span><div><h3>VELDU</h3></div></li>
-            <li><span>Image 05</span><div><h3>ÞÍNA</h3></div></li>
-            <li><span>Image 06</span><div><h3>LÍKAMSRÆKT!</h3></div></li>
+            <li><span>Image 01</span><div><h3>BYRJAÐU STRAX Í DAG!</h3></div></li>
+            <li><span>Image 02</span><div><h3>VELDU ÞÍNA LÍKAMSRÆKT!</h3></div></li>
+            <li><span>Image 03</span><div><h3>BYRJAÐU STRAX Í DAG!</h3></div></li>
+            <li><span>Image 04</span><div><h3>VELDU ÞÍNA LÍKAMSRÆKT!</h3></div></li>
+            <li><span>Image 05</span><div><h3>BYRJAÐU STRAX Í DAG!</h3></div></li>
+            <li><span>Image 06</span><div><h3>VELDU ÞÍNA LÍKAMSRÆKT!</h3></div></li>
         </ul>
 		</div>
 
@@ -39,7 +39,7 @@
 				    
 			    </div>
 			    <div class="col-md-3 col-sm-6">
-			    	<h2>Umsögn 3</h2>
+			    	<h2>Spinning er ekki fyrir alla!</h2>
 				    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
 				    Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
 				    dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
@@ -48,7 +48,7 @@
 				    
 			    </div>
 			    <div class="col-md-3 col-sm-6">
-			    	<h2>Umsögn 4</h2>
+			    	<h2>11 tæki sem þú hefur aldrei prófað</h2>
 				    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
 				    Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
 				    dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
@@ -60,19 +60,15 @@
 		  </div>
 		  <hr>
 		  <div class="row">
-		    <h2>Grein</h2>
-		    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-		    Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
-		    dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
-		    Aliquam in felis sit amet augue.
-		    <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-		    Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
-		    dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
-		    Aliquam in felis sit amet augue.</p>
-		    <a href="#" class="btn btn-default">Lesa</a>
-		    
+		  <div class="col-md-12 col-sm-12">
+
+		    <h2>Bera saman líkamsræktir</h2>
+		    <p>Það er oft erfitt að ákveða sig hvaða líkamsræktar stöð hentar þér best. Staðsetning, stærð og verð spila þar stóran þátt. 
+		    	<br>-Er betra að velja stöð með hærra mánaðargjald ef hún er nærri? <br>-Villtu fara og lyfta bara lóðum eða villtu taka spinning tíma?
+		    	 <br>Hér er hægt að bera saman þessa valkosti á einni síðu án þess að þurfa flakka á milli heimasíðna stöðvana og leita að þeim upplýsingum.</p>
+		    <a href="samanburdur.php" class="btn btn-default">Bera saman</a>
+		      </div>
 		  </div>
-		  <hr>
 		</div>
 		 ';
 
